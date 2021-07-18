0df2083599f1d73254bec4f67421d51084ea8365
Natig2992 <nagiev2992@mail.ru>
2021-07-18 03:06:49 +0300

    Contunie to ALB Load Balancer configuration

--- main.tf
+++ main.tf
@@ -14,7 +14,17 @@ resource "aws_security_group" "terraform-sec-instance" {
         to_port   = 8080
         protocol  = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
+
     }
+   ingress {
+       
+       from_port = 22 
+       to_port = 22
+       protocol = "tcp"
+       cidr_blocks = ["0.0.0.0/0"]
+
+
+   } 
 }
 
     
@@ -59,8 +69,8 @@ data "aws_subnet_ids" "default" {
 
 #Example of launch configuration:
 resource "aws_launch_configuration" "terraform-example-launch" {
-
-    image_id = "ami-0c55b159cbfafe1f0"
+    name = "test-for-web"
+    image_id = "ami-05f7491af5eef733a"
     instance_type = "t2.micro"
     security_groups = [aws_security_group.terraform-sec-instance.id]
     user_data = <<-EOF
@@ -72,6 +82,7 @@ resource "aws_launch_configuration" "terraform-example-launch" {
     lifecycle {
         create_before_destroy = true
     }
+
 }
 
 #Example of Auto Scaling group:
@@ -81,6 +92,8 @@ resource "aws_autoscaling_group" "autoscale-example" {
     min_size = 2
     max_size = 10
     vpc_zone_identifier = data.aws_subnet_ids.default.ids #Argument for subnet_ids 
+    target_group_arns = [aws_lb_target_group.asg.arn]
+    health_check_type = "ELB"
     tag {
         key = "Name"
         value = "terraform-asgexample"
@@ -89,7 +102,95 @@ resource "aws_autoscaling_group" "autoscale-example" {
 
 }
 
+#Create an ALB load-balancer for our app servers:
 
+resource "aws_lb" "aws_example" {
 
+    name               = "terrafor-alb-example"
+    load_balancer_type = "application"
+    subnets            = data.aws_subnet_ids.default.ids
+    security_groups    = [aws_security_group.alb.id]
+}
 
+resource "aws_lb_listener" "http" {
 
+    load_balancer_arn = aws_lb.example.arn
+    port              = 80
+    protocol          = "HTTP"
+
+#By default return empty string with 404 code
+
+    default_action {
+
+        type = "fixed-response"
+
+        fixed_response {
+
+            content_type = "text/plain"
+            message_body = "404: page not found"
+            status_code  = 404
+
+        }
+    }
+}
+#Example of a security_group for our ALB:
+resource "aws_security_group" alb {
+
+    name = "terraform-example-alb-sg"
+    ingress {
+        from_port = 80
+        to_port   = 80
+        protocol  = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = 0
+        to port   = 0 
+        protocol  = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+}
+
+#Example of a target_group for out ASG:
+
+resource "aws_lb_target_group" "asg" {
+    name = "terraform-asg-example01"
+    port = var.server_port
+    protocol = "HTTP"
+    vpc_id = data.aws_vpc.default.id
+    health_check {
+        path = "/"
+        protocol = "HTTP"
+        matcher = "200"
+        interval = 15
+        timeout = 3
+        healthy_threshold = 2
+        unhealthy_threshold = 2
+
+    }
+}
+
+#Example of a create aws-lb-listener-rule:
+
+resource "aws-lb-listener-rule" "asg" {
+
+    listener_arn = aws_lb_listener.http.arn
+    priority = 100
+    condition {
+        field = "path-pattern"
+        values = ["*"]
+    }
+    action {
+        type = "forward"
+        target_group_arn = aws_lb_target_group.asg.arn
+         
+    }
+}
+
+output "alb_dns_name" {
+
+    value       = aws_lb.example.dns_name
+    description = "The domain name of the load balancer"
+
+}
